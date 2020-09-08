0e758df52439c6c81d75b912021b3a1cd29e94e1
Pieter Zitman <pieterzitman@hotmail.com>
2020-09-08 22:30:07 +0200

    small changes

--- docs/main.js
+++ docs/main.js
@@ -18,7 +18,7 @@ start()
 function createBreedList(breedList) {
     document.getElementById("breed").innerHTML = `
     <select onchange="loadByBreed(this.value)">
-        <option>Choose a dog breed</option>
+        <option>Choose a dog breed!</option>
         ${Object.keys(breedList).map(function (breed) {
             return `<option>${breed}</option>`
         }).join('')}
@@ -27,7 +27,7 @@ function createBreedList(breedList) {
 }
 
 async function loadByBreed(breed) {
-    if (breed != "Choose a dog breed") {
+    if (breed != "Choose a dog breed!") {
         const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`)
         const data = await response.json()
         createSlideshow(data.message)
